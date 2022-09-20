01573166fcd2922b7d90548e63e83e910aa59185
Yevhen Murzik <emourzik@gmail.com>
2022-09-20 23:05:41 +0200

    First commit

---
+++ components/Input/input.js
@@ -0,0 +1,5 @@
+export const Input = () => {
+    return `
+        <input class="center" type="text">
+    `
+} 
---
+++ main.js
@@ -0,0 +1,7 @@
+// imports
+import { Input } from "./components/Input/input.js"
+
+// main code
+
+const root = document.getElementById("root");
+root.innerHTML = Input()
